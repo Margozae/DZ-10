0410d13755be7a86cae873369821e2b542f0f0fe
Margozae <59208589+Margozae@users.noreply.github.com>
2020-02-29 20:10:41 +0200

    Add files via upload

---
+++ list/src/index.js
@@ -0,0 +1,24 @@
+function generateList(arr) {
+    const ul = document.createElement('ul');
+    document.body.append(ul);
+    for(let i=0; i < arr.length; i++) {
+        if (typeof(arr[i]) !== 'object') {
+            const li = document.createElement('li');
+            li.append(arr[i]);
+            ul.append(li);
+        } else if (typeof(arr[i]) === 'object') {
+            const item = arr[i];
+            const li = document.createElement('li');
+            const innerUl = document.createElement('ul');
+            li.append(innerUl);
+            ul.append(li);
+            for(let j = 0; j < item.length; j++) {
+                const innerli = document.createElement('li');
+                innerli.innerText = item[j];
+                innerUl.append(innerli);
+            }
+        }
+    }
+}
+
+generateList([1, 2, [1.1,1.2,1.3], 3]);   // eslint-disable-line 
