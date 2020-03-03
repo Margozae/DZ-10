cc4238af5f15232b1788baaed2b16d1bd9a55d84
Margozae <59208589+Margozae@users.noreply.github.com>
2020-03-03 16:12:08 +0200

    Add files via upload

---
+++ table/src/index.js
@@ -0,0 +1,18 @@
+
+function foo (limit) {
+    const table = document.createElement('table');
+    document.body.append(table);
+    for (let i = 0; i < limit; i++) {
+        const tr = document.createElement('tr');
+        table.append(tr);
+        for (let j = 1; j <= limit; j++) {
+            const td = document.createElement('td');
+            tr.append(td);
+            td.style.border = '1px solid #000000';
+            td.style.padding = '20px';
+            td.append(j + i * limit);
+        }
+    }
+}
+
+foo(10); // eslint-disable-line 
